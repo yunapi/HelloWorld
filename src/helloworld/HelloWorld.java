4e0bf5bdc4141b61bc7e2d3b4399e1a27cbc4272
g12936ny <g12936ny@s30446.tsuda.ac.jp>
2014-04-11 11:03:36 +0900

    新規にHelloWorld

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author g12936ny
+ */
+public class HelloWorld {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
